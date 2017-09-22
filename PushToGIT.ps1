b3457d1ce1257145a7b7bf8728003a951acc3495
BRAMHALL <denabramhall@northwesternmutual.com>
2017-09-22 14:10:34 -0600

    DAB changes

--- PushToGIT.ps1
+++ PushToGIT.ps1
@@ -1,10 +1,13 @@
 ﻿#Push Changes to GIT
 #Prerequisites: Add environment variable 'Path' for Git
 #Example: C:\Program Files\Git\cmd
-
+Param
+  (
+  [parameter(mandatory=$true)]$Gitpush
+  )
 $msg = Read-Host "Enter Commit Message"
 
 git add *
 git commit -a -m $msg
 #Add a prompt for which repo
-git push --set-upstream https://github.com/dbramhall/GitPowershell.git master
+#git push --set-upstream https://github.com/dbramhall/GitPowershell.git master
